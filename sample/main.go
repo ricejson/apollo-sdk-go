bb24dfefddcf2ae96ba5c83ad8291235a9fcede6
ricejson <wjjbangbangbang@163.com>
2026-01-17 21:14:52 +0800

    修改SDK-Go示例代码

---
+++ sample/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"github.com/ricejson/apollo-sdk-go/client"
+	"github.com/ricejson/apollo-sdk-go/model"
+)
+
+func main() {
+	// 创建客户端实例
+	client := client.NewClient()
+	// 构造condition
+	user := model.NewUser().
+		With("user_id", "123").
+		With("city", "Beijing")
+	// 获取结果
+	allow, err := client.IsToggleAllowV2(context.Background(), "tg_wri5tl24n", "123", user)
+	if err != nil {
+		// 处理错误逻辑
+	}
+	if allow {
+		// 执行业务操作
+	}
+}
